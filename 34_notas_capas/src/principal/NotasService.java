0d8b3be715f04bda3acc75dc66fed49cc00f8df1
manana <manana@A13E32>
2024-04-02 13:57:40 +0200

    I LIKE IT!!

---
+++ 34_notas_capas/src/principal/NotasService.java
@@ -0,0 +1,37 @@
+package principal;
+
+import java.util.ArrayList;
+////Realizar una nueva versión del programa de las notas. Al iniciar el programa
+//aparecerá el siguiente menú:
+//1.- Nueva nota
+//2.- Calcular media
+//3.- Notas extremas
+//4.- Ver todas
+//5.- Salir 
+public class NotasService {
+	ArrayList<Double> notas= new ArrayList<Double>();
+	public void agregarNota(double nota) {
+		
+	}
+	public double media() {
+		
+	}
+	
+	public double max() {
+		
+	}
+	
+	public double min( ) {
+		
+	}
+	
+	public double [] obtenerNotas() {
+		
+	}
+	
+
+}
+
+
+
+
